b887c349a7c1474abbd15ef1457f5db7fbe961ff
Lordey <maxime.lordey@gmail.com>
2018-03-17 21:01:02 +0100

    src/

--- TP2_Blas/TPBLAS/src/axpy.c
+++ TP2_Blas/TPBLAS/src/axpy.c
@@ -37,8 +37,7 @@ void mncblas_caxpy(const int N, const void* a, const void *X, const int incX,
 
   for (; ((i < N) && (j < N)) ; i += incX, j += incY)
     {
-      ((struct complex_simple*)Y)[j].real = ((struct complex_simple*)a)->real * ((struct complex_simple*)X)[i].real + ((struct complex_simple*)Y)[j].real;
-      ((struct complex_simple*)Y)[j].imaginary = ((struct complex_simple*)a)->imaginary * ((struct complex_simple*)X)[i].imaginary + ((struct complex_simple*)Y)[j].imaginary ;
+      ((struct complex_simple*)Y)[j] = addition_cs( multiplication_cs ( *((struct complex_simple*)a) , ((struct complex_simple*)X)[i] ) , ((struct complex_simple*)Y)[j] );
     }
 
   return ;
@@ -52,8 +51,7 @@ void mncblas_zaxpy(const int N, const void* a, const void *X, const int incX,
 
   for (; ((i < N) && (j < N)) ; i += incX, j += incY)
     {
-      ((struct complex_double*)Y)[j].real = ((struct complex_double*)a)->real * ((struct complex_double*)X)[i].real + ((struct complex_double*)Y)[j].real;
-      ((struct complex_double*)Y)[j].imaginary =((struct complex_double*)a)->imaginary * ((struct complex_double*)X)[i].imaginary + ((struct complex_double*)Y)[j].imaginary ;
+      ((struct complex_double*)Y)[j] = addition_cd( multiplication_cd ( *((struct complex_double*)a) , ((struct complex_double*)X)[i] ) , ((struct complex_double*)Y)[j] );
     }
 
   return ;
