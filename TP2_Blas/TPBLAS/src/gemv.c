94c9724256486872c0043424fc1ecd846b009c08
Lordey <maxime.lordey@gmail.com>
2018-03-19 20:10:13 +0100

    LMAO KENT1 IL RAGE

--- TP2_Blas/TPBLAS/src/gemv.c
+++ TP2_Blas/TPBLAS/src/gemv.c
@@ -30,12 +30,12 @@ void mncblas_dgemv (MNCBLAS_LAYOUT layout,
                  const double *X, const int incX, const double beta,
                  double *Y, const int incY)
 {
-  register unsigned int i;
+	register unsigned int i;
   register unsigned int j;
 
   double tmp ;
   for (i = 0 ; i < M ; i += incX) {
-    tmp = A[0+N*i] * X[0] ;
+    tmp = A[i*N] * X[0] ;
     for (j=1; j < N ;j += incY) {
       tmp +=  A[j+N*i] * X[j] ;
     }
@@ -52,36 +52,22 @@ void mncblas_cgemv (MNCBLAS_LAYOUT layout,
                  const void *X, const int incX, const void *beta,
                  void *Y, const int incY)
 {
-register unsigned int i = 0 ;
-register unsigned int j = 0 ;
+	register unsigned int i;
+  register unsigned int j;
 
-struct complex_simple *tmp = malloc(M * sizeof(struct complex_simple));
-for (; j < N ; j += incY) {
-  tmp[j].real = ((struct complex_simple*)Y)[j].real;
-  tmp[j].imaginary = ((struct complex_simple*)Y)[j].imaginary;
-}
-
-struct complex_simple res;
-for (; i < M ; i += incX) {
-
-    res.real = 0.;
-	res.imaginary = 0.;
-
-    for (j=0; j < N ;j += incY) {
-      res = addition_cs(res,addition_cs(
-								multiplication_cs(
-										multiplication_cs( *((struct complex_simple*)alpha) , ((struct complex_simple*)A)[j+N*i])
-										,((struct complex_simple*)X)[j]
-								),
-								multiplication_cs( *((struct complex_simple*)beta) , tmp[j] )
-							)
-			);
+  struct complex_simple tmp ;
+  for (i = 0 ; i < M ; i += incX) {
+    tmp = multiplication_cs( ((struct complex_simple*)A)[i*N] , ((struct complex_simple*)X)[0] );
+    for (j=1; j < N ;j += incY) {
+      tmp =addition_cs(tmp ,  multiplication_cs( ((struct complex_simple*)A)[j+i*N] , ((struct complex_simple*)X)[j] ) );
     }
-    ((struct complex_simple*)Y)[i] = res;
+    ((struct complex_simple*)Y)[i] = addition_cs( multiplication_cs( *((struct complex_simple*)alpha) , tmp )
+																									,
+																								 multiplication_cs( *((struct complex_simple*)beta) , ((struct complex_simple*)Y)[i] )
+																								);
   }
 
-free(tmp);
-return;
+  return;
 }
 
 void mncblas_zgemv (MNCBLAS_LAYOUT layout,
@@ -90,33 +76,20 @@ void mncblas_zgemv (MNCBLAS_LAYOUT layout,
                  const void *X, const int incX, const void *beta,
                  void *Y, const int incY)
 {
-  register unsigned int i = 0 ;
-  register unsigned int j = 0 ;
+	register unsigned int i;
+  register unsigned int j;
 
-struct complex_double *tmp = malloc(M * sizeof(struct complex_double));
-  for (; j < N ; j += incY) {
-    tmp[j].real = ((struct complex_double*)Y)[j].real;
-    tmp[j].imaginary = ((struct complex_double*)Y)[j].imaginary;
-  }
-
-struct complex_double res;
-for (; i < M ; i += incX) {
-
-    res.real = 0.;
-	res.imaginary = 0.;
-
-    for (j=0; j < N ;j += incY) {
-      res = addition_cd(res,addition_cd(
-								multiplication_cd(
-										multiplication_cd( *((struct complex_double*)alpha) , ((struct complex_double*)A)[j+N*i])
-										,((struct complex_double*)X)[j]
-								),
-								multiplication_cd( *((struct complex_double*)beta) , tmp[j] )
-							)
-			);
+  struct complex_double tmp ;
+  for (i = 0 ; i < M ; i += incX) {
+    tmp = multiplication_cd( ((struct complex_double*)A)[i*N] , ((struct complex_double*)X)[0] );
+    for (j=1; j < N ;j += incY) {
+      tmp = addition_cd(tmp ,  multiplication_cd( ((struct complex_double*)A)[j+i*N] , ((struct complex_double*)X)[j] ) );
     }
-    ((struct complex_double*)Y)[i] = res;
+    ((struct complex_double*)Y)[i] = addition_cd( multiplication_cd( *((struct complex_double*)alpha) , tmp )
+																									,
+																								 multiplication_cd( *((struct complex_double*)beta) , ((struct complex_double*)Y)[i] )
+																								);
   }
-  free(tmp);
+
   return;
 }
